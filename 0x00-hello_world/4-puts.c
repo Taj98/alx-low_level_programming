785b6bbb11e7f2104af34046801993ea5b7f4421
Taj98 <chillzy98@gmail.com>
2022-07-04 12:56:20 +0200

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle and trying to solve it in different ways\" - Niklaus Wirth");
+    puts("Programming is like building a multilingual puzzle");
     return (0);
 }
